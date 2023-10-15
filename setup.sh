4adc58594235c3a77d4ac4ce48ab767363939770
micael-sjogren <101263696+micael-sjogren@users.noreply.github.com>
2023-10-15 10:49:19 +0200

    Update setup.sh
    added raw path instead of html

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-sudo curl -o /home/docker_cleanup.sh "https://github.com/micael-sjogren/cicd/blob/main/docker_cleanup"
+sudo curl -o /home/docker_cleanup.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_cleanup"
 chmod +x /home/docker_cleanup.sh
 
-sudo curl -o /home/docker_firewall.sh "https://github.com/micael-sjogren/cicd/blob/main/docker_firewall"
+sudo curl -o /home/docker_firewall.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_firewall"
 chmod +x /home/docker_firewall.sh
 
-sudo curl -o /home/docker_install.sh "https://github.com/micael-sjogren/cicd/blob/main/docker_install"
+sudo curl -o /home/docker_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_install"
 chmod +x /home/docker_install.sh
 
-sudo curl -o /home/docker_jenkins_install.sh "https://github.com/micael-sjogren/cicd/blob/main/docker_jenkins_install"
+sudo curl -o /home/docker_jenkins_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_jenkins_install"
 chmod +x /home/docker_jenkins_install.sh
 
-sudo curl -o /home/docker_grafana_install.sh "https://github.com/micael-sjogren/cicd/blob/main/docker_grafana_install"
+sudo curl -o /home/docker_grafana_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_grafana_install"
 chmod +x /home/docker_grafana_install.sh
 
-sudo curl -o /etc/prometheus/prometheus.yml "https://github.com/micael-sjogren/cicd/blob/main/prometheus.yml"
-sudo curl -o /home/docker_prometheus_install.sh "https://github.com/micael-sjogren/cicd/blob/main/docker_prometheus_install"
+sudo curl -o /etc/prometheus/prometheus.yml "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/prometheus.yml"
+sudo curl -o /home/docker_prometheus_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_prometheus_install"
 chmod +x /home/docker_prometheus_install.sh
 
 
