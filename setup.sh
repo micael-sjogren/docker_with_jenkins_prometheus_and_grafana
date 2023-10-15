6d65cfd5df21b1b4ccc57010b187487cd99569a3
micael-sjogren <101263696+micael-sjogren@users.noreply.github.com>
2023-10-15 10:58:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ chmod +x docker_jenkins_install.sh
 sudo curl -o docker_grafana_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_grafana_install"
 chmod +x docker_grafana_install.sh
 
-sudo curl -o /etc/prometheus/prometheus.yml "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/prometheus.yml"
-sudo curl -o /home/docker_prometheus_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_prometheus_install"
-chmod +x /home/docker_prometheus_install.sh
+sudo curl -o prometheus.yml "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/prometheus.yml"
+sudo curl -o docker_prometheus_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/blob/main/docker_prometheus_install"
+chmod +x docker_prometheus_install.sh
 
 
 sudo ./docker_cleanup.sh
