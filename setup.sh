22351f410039c0d30c731ba5a87fb8ae6c6af514
micael-sjogren <101263696+micael-sjogren@users.noreply.github.com>
2023-10-15 11:40:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 sudo mkdir -p /etc/prometheus/
-
-sudo curl -o /home/$SUDO_USER/docker_cleanup.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_cleanup"
-sudo curl -o /home/$SUDO_USER/docker_firewall.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_firewall"
-sudo curl -o /home/$SUDO_USER/docker_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_install"
-sudo curl -o /home/$SUDO_USER/docker_jenkins_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_jenkins_install"
-sudo curl -o /home/$SUDO_USER/docker_grafana_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_grafana_install"
-sudo curl -o /home/$SUDO_USER/prometheus.yml "https://raw.githubusercontent.com/micael-sjogren/cicd/main/prometheus.yml"
-sudo curl -o /home/$SUDO_USER/docker_prometheus_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_prometheus_install"
+ 
+sudo curl -o /home/$SUDO_USER/docker_cleanup.sh "https://raw.githubusercontent.com/micael-sjogren/docker_with_jenkins_prometheus_and_grafana/main/docker_cleanup"
+sudo curl -o /home/$SUDO_USER/docker_firewall.sh "https://raw.githubusercontent.com/micael-sjogren/docker_with_jenkins_prometheus_and_grafana/main/docker_firewall"
+sudo curl -o /home/$SUDO_USER/docker_install.sh "https://raw.githubusercontent.com/micael-sjogren/docker_with_jenkins_prometheus_and_grafana/main/docker_install"
+sudo curl -o /home/$SUDO_USER/docker_jenkins_install.sh "https://raw.githubusercontent.com/micael-sjogren/docker_with_jenkins_prometheus_and_grafana/main/docker_jenkins_install"
+sudo curl -o /home/$SUDO_USER/docker_grafana_install.sh "https://raw.githubusercontent.com/micael-sjogren/docker_with_jenkins_prometheus_and_grafana/main/docker_grafana_install"
+sudo curl -o /home/$SUDO_USER/prometheus.yml "https://raw.githubusercontent.com/micael-sjogren/docker_with_jenkins_prometheus_and_grafana/main/prometheus.yml"
+sudo curl -o /home/$SUDO_USER/docker_prometheus_install.sh "https://raw.githubusercontent.com/micael-sjogren/docker_with_jenkins_prometheus_and_grafana/main/docker_prometheus_install"
 
 sudo chmod +x /home/$SUDO_USER/docker_cleanup.sh
 sudo chmod +x /home/$SUDO_USER/docker_firewall.sh
