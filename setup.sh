81dd112bdab6c4728c4033008466acfd4a382682
micael-sjogren <101263696+micael-sjogren@users.noreply.github.com>
2023-10-15 11:16:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,32 +1,25 @@
 #!/usr/bin/env bash
 
-# Ensure prometheus directory exists
 sudo mkdir -p /etc/prometheus/
 
-# Verify script URLs and download them
-sudo curl -o /home/$USER/docker_cleanup.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_cleanup"
-sudo curl -o /home/$USER/docker_firewall.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_firewall"
-sudo curl -o /home/$USER/docker_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_install"
-sudo curl -o /home/$USER/docker_jenkins_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_jenkins_install"
-sudo curl -o /home/$USER/docker_grafana_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_grafana_install"
+sudo curl -o /home/$SUDO_USER/docker_cleanup.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_cleanup"
+sudo curl -o /home/$SUDO_USER/docker_firewall.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_firewall"
+sudo curl -o /home/$SUDO_USER/docker_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_install"
+sudo curl -o /home/$SUDO_USER/docker_jenkins_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_jenkins_install"
+sudo curl -o /home/$SUDO_USER/docker_grafana_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_grafana_install"
 sudo curl -o /etc/prometheus/prometheus.yml "https://raw.githubusercontent.com/micael-sjogren/cicd/main/prometheus.yml"
-sudo curl -o /home/$USER/docker_prometheus_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_prometheus_install"
+sudo curl -o /home/$SUDO_USER/docker_prometheus_install.sh "https://raw.githubusercontent.com/micael-sjogren/cicd/main/docker_prometheus_install"
 
-# Make scripts executable
-sudo chmod +x /home/$USER/docker_cleanup.sh
-sudo chmod +x /home/$USER/docker_firewall.sh
-sudo chmod +x /home/$USER/docker_install.sh
-sudo chmod +x /home/$USER/docker_jenkins_install.sh
-sudo chmod +x /home/$USER/docker_grafana_install.sh
-sudo chmod +x /home/$USER/docker_prometheus_install.sh
+sudo chmod +x /home/$SUDO_USER/docker_cleanup.sh
+sudo chmod +x /home/$SUDO_USER/docker_firewall.sh
+sudo chmod +x /home/$SUDO_USER/docker_install.sh
+sudo chmod +x /home/$SUDO_USER/docker_jenkins_install.sh
+sudo chmod +x /home/$SUDO_USER/docker_grafana_install.sh
+sudo chmod +x /home/$SUDO_USER/docker_prometheus_install.sh
 
-# Execute scripts
-sudo /home/$USER/docker_cleanup.sh
-sudo /home/$USER/docker_install.sh
-sudo /home/$USER/docker_jenkins_install.sh
-sudo /home/$USER/docker_grafana_install.sh
-sudo /home/$USER/docker_prometheus_install.sh
-sudo /home/$USER/docker_firewall.sh
-
-
-
+sudo /home/$SUDO_USER/docker_cleanup.sh
+sudo /home/$SUDO_USER/docker_install.sh
+sudo /home/$SUDO_USER/docker_jenkins_install.sh
+sudo /home/$SUDO_USER/docker_grafana_install.sh
+sudo /home/$SUDO_USER/docker_prometheus_install.sh
+sudo /home/$SUDO_USER/docker_firewall.sh
